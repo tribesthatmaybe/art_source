fca439d2c7b2066e93a5fe85b986b42bf6775ca1
Tyler Jachetta <tyler.jachetta@ltnglobal.com>
2020-08-30 11:50:21 -0400

    Build sidecar parsing: Added really unnecessary merge function because I thought it might be fun to do some Sunday morning nerdsniping

--- build_tools/export_maps.py
+++ build_tools/export_maps.py
@@ -1,12 +1,22 @@
+import collections
+import copy
 import gimpfu
 import os
 import os.path
+import six
 import sys
 import toml
 
 import logging
 
+if six.PY3:
+    import abc.abc as abc
+
+else:
+    import collections as abc
+
 TOML_EXT = '.toml'
+ALL_LAYERS = 'all'
 
 logger = logging.getLogger(__name__)
 
@@ -36,6 +46,70 @@ def process(path):
     return results
 
 
+def _is_collection(item):
+    # Python2 doesn't have collection (sized, iterable container)
+
+    if six.PY3 and isinstance(item, collections.Collection):
+        return True
+    elif six.PY2 and isinstance(item, collections.Iterable) and isinstance(item, collections.Container) and isinstance(item, collections.Sized):
+        return True
+    return False
+
+
+def _merge(v1, v2):
+    """
+    Do a deep merge of two dictionaries.
+    If this encounters a hash or a collection, it will handle the values by
+    merging them (preferring d2's values when there are two values at the same
+    key).
+
+    Args:
+        d1 (dict): The base dictionary into which to merge another dictionary
+        d2 (dict): The dictionary to merge into d1, overwriting values that
+                   conflict
+
+    """
+
+    print('merging {} and {}'.format(v1, v2))
+
+    if six.PY2:
+        handled_abcs = (abc.Mapping,)
+    else:
+        handled_abcs = (abc.Mapping, abc.Collection)
+
+    if isinstance(v1, abc.Mapping) and isinstance(v2, abc.Mapping):
+        res = {}
+        for k in set(v1.keys()) | set(v2.keys()):
+            print('checking key ' + k)
+            if k not in v2:
+                res[k] = v1[k]
+            elif k not in v1:
+                res[k] = v2[k]
+            else:
+                kr = _merge(v1[k], v2[k])
+                res[k] = kr
+        return res
+
+    # Possible it could be a non-sequential collection, in which case comparing
+    # this way would be nonsensical
+    elif isinstance(v1, abc.Sequence) and isinstance(v2, abc.Sequence):
+        res = list(v1)
+        for idx, element in enumerate(v2):
+            if idx >= len(res):
+                res.append(element)
+            elif isinstance(element, handled_abcs) or _is_collection(element):
+                res[idx] = _merge(v1[idx], v2[idx])
+            else:
+                # non-merged type, add to list
+                if element not in res:
+                    res.append(element)
+        return res
+
+    elif _is_collection(v1) and _is_collection(v2):
+        return list(v1) + [el for el in v2 if el not in v1]
+
+    # Neither a mapping nor a collection, just use v2
+    return v2
 def handle(gimp_filepath, sidecar_filepath):
 
     image = gimpfu.pdb.gimp_file_load(
