ba444aa26fc7ebc04cf3ab937238b021c6b15fcd
Tyler Jachetta <tyler.jachetta@ltnglobal.com>
2020-08-30 13:21:33 -0400

    Setup: Fixed setup script

--- build_tools/setup.py
+++ build_tools/setup.py
@@ -1,8 +1,9 @@
 import setuptools
 
-
 setuptools.setup(
-    'tribes_art_build_tools',
+    name='tribes_art_build_tools',
     version='0.1',
     py_modules=['export_maps'],
+    # ToDo differentiate for py3
+    install_requires=['six', 'toml']
 )
