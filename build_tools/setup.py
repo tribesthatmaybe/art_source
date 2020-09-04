d0551f373de4c760de9569b7c6a3981443a292e0
Tyler Jachetta <tyler.jachetta@ltnglobal.com>
2020-09-04 07:41:02 -0400

    Version: Made version something reasonable and dependable.

--- build_tools/setup.py
+++ build_tools/setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='tribes_art_build_tools',
-    version='0.1',
+    version='13',
     py_modules=['export_maps'],
     # ToDo differentiate for py3
     install_requires=['six', 'toml']
